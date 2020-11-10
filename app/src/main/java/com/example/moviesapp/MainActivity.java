88095a8bd63e6aec28fff8b3ec88ddc14e2fa7b2
Rodolfo Rojas <jrodolforojasguzman@gmail.com>
2020-11-10 10:08:11 -0600

    Create ListView in Main Activity

--- app/src/main/java/com/example/moviesapp/MainActivity.java
+++ app/src/main/java/com/example/moviesapp/MainActivity.java
@@ -1,23 +1,36 @@
 package com.example.moviesapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tomatoMeter;
+    ListView listView;
+    ArrayList<Movie> movies = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tomatoMeter = findViewById(R.id.textView);
+        listView = findViewById(R.id.listView);
+        MyApadter myApadter = new MyApadter(this,movies);
         WebScraping webScraping = new WebScraping();
         Movie newMovie = new Movie();
-
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -25,8 +38,19 @@ public class MainActivity extends AppCompatActivity {
                 webScraping.getGooglePlayInfo(newMovie);
                 webScraping.getAppleTVInfo(newMovie);
                 printMovieInfo(newMovie);
+                movies.add(newMovie);
+
             }
+
+
         }).start();
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                listView.setAdapter(myApadter);
+            }
+        });
     }
 
     private void printMovieInfo(Movie movie){
@@ -42,6 +66,36 @@ public class MainActivity extends AppCompatActivity {
                 "CATEGORY: " + "FALTA" + "\n" +
                 "Video: " + "\n"
         );
+    }
+
+    class MyApadter extends ArrayAdapter<Movie>{
+        Context context;
+        ArrayList<Movie> movies;
+
+        public MyApadter(Context context, ArrayList<Movie> movies){
+            super(context,R.layout.row);
+            this.movies = movies;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext().
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View row = layoutInflater.inflate(R.layout.row, parent, false);
+
+            ImageView image = row.findViewById(R.id.image);
+            TextView title = row.findViewById(R.id.title);
+            TextView category = row.findViewById(R.id.category);
+            TextView price = row.findViewById(R.id.price);
+
+            title.setText(movies.get(position).getName());
+            category.setText(movies.get(position).getCategory());
+            price.setText(movies.get(position).getAppleTVPrice());
+
+            return row;
+        }
     }
 
 }
